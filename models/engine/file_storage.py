70dd4c10550744f702e90810ed50057ef8bc10f7
mukarama-farouk <faroukmukarama@gmail.com>
2023-08-08 11:50:19 -0700

    file-storage.py was updated with the save(self) and new(self) methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -21,3 +21,22 @@ class FileStorage:
         """Public instance method of class FileStorage that returns
         the dictionary __objects"""
         return FileStorage.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        obj_class_name = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(obj_class_name, obj.id)] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file"""
+        objects_dict = FileStorage.__objects
+        serialized_objects = {}
+
+        for obj_key in objects_dict:
+            obj_instance = objects_dict[obj_key]
+            serialized_objects[obj_key] = obj_instance.to_dict()
+
+        with open(FileStorage.__objects, 'w') as f:
+            json.dump(serialized_objects, f)
+
+        
