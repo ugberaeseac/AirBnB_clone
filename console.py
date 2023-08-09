42cf8286118d310d06568b5b0d6e1056c849b7df
mukarama-farouk <faroukmukarama@gmail.com>
2023-08-09 03:39:58 -0700

    Created the CLI and added some commands(create)

---
+++ console.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""This is a module  that contains the entry point of the command interpreter
+"""
+import cmd
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+from models import storage
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command Interpreter class"""
+
+    prompt = "(hbnb) "
+
+    class_names = {
+
+            "BaseModel": BaseModel,
+            "User": User,
+            "State": State,
+            "City": City,
+            "Place": Place,
+            "Amenity": Amenity,
+            "Review": Review
+    }
+
+
+    def emptyline(self):
+        """Do nothing upon receiving an empty line."""
+        pass
+
+    def do_quit(self, args):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, args):
+        "Exits the program and prints a new line before exiting"""
+        print(" ")
+        return True
+
+    def do_create(self, args):
+        """Creates a new instance of BaseModel, saves it (to the JSON file)
+        and prints the id"""
+        if args == "":
+            print("** class name missing **")
+            return
+
+        class_name = args
+        if class_name in self.class_names:
+            new_obj = self.class_names[class_name]()
+            new_obj.save()
+            print(new_obj.id)
+        else:
+            print("** class doesn't exist **")
+
+     
+     def do_show(self, args):
+         """ Prints the string representation of an instance based
+         on the class name and id"""
+         if args == "":
+             print("** class name missing **")
+             return
+
+
+        
+
+
+
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
